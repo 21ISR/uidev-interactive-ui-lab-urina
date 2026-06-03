3e7f6c45b54c04932d24ec98ea490a127c90617c
MC-ZLATKO <aaconlt089@gmail.com>
2026-06-03 06:21:25 +0000

    o

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const popupOverlay = document.querySelector('.popup-overlay')
+    const openPopupBtn = document.querySelector('#show-popup')
+    const closePopupBtn = document.querySelector('.popup-close')
+    const popupForm = document.querySelector('#popup-form')
+
+    function openPopup() {
+        popupOverlay.style.display = 'flex'
+    }
+
+    function closePopup() {
+        popupOverlay.style.display = 'none'
+    }
+
+    if (openPopupBtn && popupOverlay) {
+        openPopupBtn.addEventListener('click', openPopup)
+    }
+
+    if (closePopupBtn) {
+        closePopupBtn.addEventListener('click', closePopup)
+    }
+
+    if (popupOverlay) {
+        popupOverlay.addEventListener('click', (event) => {
+            if (event.target === popupOverlay) {
+                closePopup()
+            }
+        })
+    }
+
+    if (popupForm) {
+        popupForm.addEventListener('click', (event) => {
+            event.preventDefault()
+            alert('Сообщение отправлено')
+            closePopup()
+            popupForm.reset()
+        })
+    }
+
+    
+})
