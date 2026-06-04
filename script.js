7d40e9da6fbd2d460824682a6a573a39a864fd03
MC-ZLATKO <aaconlt089@gmail.com>
2026-06-04 05:54:11 +0000

    ;;

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
     const popupOverlay = document.querySelector('.popup-overlay')
-    const openPopupBtn = document.querySelector('#show-popup')
+    const showPopupBtn = document.querySelector('#show-popup')
     const closePopupBtn = document.querySelector('.popup-close')
     const popupForm = document.querySelector('#popup-form')
 
-    function openPopup() {
-        popupOverlay.style.display = 'flex'
-    }
+//     function openPopup() {
+//         popupOverlay.style.display = 'flex'
+//     }
 
-    function closePopup() {
-        popupOverlay.style.display = 'none'
-    }
+//     function closePopup() {
+//         popupOverlay.style.display = 'none'
+//     }
 
-    if (openPopupBtn && popupOverlay) {
-        openPopupBtn.addEventListener('click', openPopup)
-    }
+//     if (openPopupBtn && popupOverlay) {
+//         openPopupBtn.addEventListener('click', openPopup)
+//     }
 
-    if (closePopupBtn) {
-        closePopupBtn.addEventListener('click', closePopup)
-    }
+//     if (closePopupBtn) {
+//         closePopupBtn.addEventListener('click', closePopup)
+//     }
 
-    if (popupOverlay) {
-        popupOverlay.addEventListener('click', (event) => {
-            if (event.target === popupOverlay) {
-                closePopup()
-            }
-        })
-    }
+//     if (popupOverlay) {
+//         popupOverlay.addEventListener('click', (event) => {
+//             if (event.target === popupOverlay) {
+//                 closePopup()
+//             }
+//         })
+//     }
 
-    if (popupForm) {
-        popupForm.addEventListener('click', (event) => {
-            event.preventDefault()
-            alert('Сообщение отправлено')
-            closePopup()
-            popupForm.reset()
-        })
-    }
+//     if (popupForm) {
+//         popupForm.addEventListener('click', (event) => {
+//             event.preventDefault()
+//             alert('Сообщение отправлено')
+//             closePopup()
+//             popupForm.reset()
+//         })
+//     }
 
     
+// })
+
+showPopupBtn.addEventListener('click', () => {
+    popupOverlay.style.display = 'flex'
+})
+
+closePopupBtn.addEventListener('click', () => {
+    popupOverlay.style.display = 'none'
+})
+
+popupOverlay.addEventListener('click', (event) => {
+    if (event.target === popupOverlay) {
+        popupOverlay.style.display = 'none'
+    }
+})
+
+popupForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    alert('Сообщение отправлено')
+    popupOverlay.style.display = 'none'
+    popupForm.reset()
 })
